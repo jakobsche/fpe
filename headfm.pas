7f6de49adff2d747b9fb263a44de29c5e459a242
Andreas Jakobsche <messages@jakobsche.de>
2020-02-05 16:06:06 +0100

    Hauptformular umbenannt und an neue Version von LCLPatch amgepaßt

---
+++ headfm.pas
@@ -0,0 +1,26 @@
+unit HeadFm;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, MasterFm;
+
+type
+  THeadForm = class(TMasterForm)
+  private
+
+  public
+
+  end;
+
+var
+  HeadForm: THeadForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
